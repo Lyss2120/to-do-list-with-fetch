a347fc1daa90bd2203a8bf992b679e31603b114c
Lyss2120 <lys.ivonne2020@gmail.com>
2022-06-30 19:47:53 -0400

    1°commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import bootstrap from 'bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container-fluid">
+      <h1>ññlll</h1>
+    </div>
+  );
+}
+
+export default App;
